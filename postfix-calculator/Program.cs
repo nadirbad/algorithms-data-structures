7c1565e97380d109592b7de2a27ae51ddef60657
Nadir Badnjevic <nadir@rubicon.ba>
2019-01-14 22:32:04 +0100

    feat(posfix-calculator): implement postfix calculator using Stack<T>

---
+++ postfix-calculator/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace postfix_calculator
+{
+  class Program
+  {
+    // Postfix calculator implementation using Stack<int>
+    // Example :
+    //   infix notation: 5 + 2
+    //   postfix notation 5 2 + (operators follow operands)
+    // Run this with CLI: dotnet run -- 5 3 9 * + 2 / 6 -
+    // Shoud return 10
+    static void Main(string[] args)
+    {
+      Stack<int> stack = new Stack<int>();
+
+      foreach (string token in args)
+      {
+        if (int.TryParse(token, out int value))
+        {
+          stack.Push(value);
+        }
+        else
+        {
+          int rightHandSide = stack.Pop();
+          int leftHandSide = stack.Pop();
+          int result;
+
+          switch (token)
+          {
+            case "*":
+              result = leftHandSide * rightHandSide;
+              break;
+            case "/":
+              result = leftHandSide / rightHandSide;
+              break;
+            case "%":
+              result = leftHandSide % rightHandSide;
+              break;
+            case "+":
+              result = leftHandSide + rightHandSide;
+              break;
+            case "-":
+              result = leftHandSide - rightHandSide;
+              break;
+            default:
+              throw new NotSupportedException("Unknown operator");
+          }
+
+          // push operation result to stack
+          stack.Push(result);
+        }
+
+      }
+
+      // Print result
+      Console.WriteLine($"Result is: {stack.Pop()}");
+
+      Console.Write("Press any key...");
+      Console.ReadKey();
+    }
+  }
+}
